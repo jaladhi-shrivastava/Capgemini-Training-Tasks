8c455fd7ef392d0a5d5f609e6f28f2b4ae04d31c
Jaladhi Shrivastava <jaladhishrivastava@gmail.com>
2026-03-19 12:40:13 +0530

    Add Spring Day 5 Tasks

---
+++ Spring_Day5 19-03-26/ContactAPI/src/main/java/com/hackerrank/stereotypes/controller/ContactController.java
@@ -0,0 +1,28 @@
+package com.hackerrank.stereotypes.controller;
+
+import com.hackerrank.stereotypes.model.Person;
+import com.hackerrank.stereotypes.service.ContactService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class ContactController {
+    ContactService contactService;
+
+    public ContactController(ContactService contactService) {
+        this.contactService = contactService;
+    }
+
+    @PostMapping("/contact/save")
+    public ResponseEntity<Person> save(@RequestBody Person person){
+        Person saved = contactService.save(person);
+        return new ResponseEntity(saved, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/contact/retrieve/{id}")
+    public ResponseEntity<Person> retrieve(@PathVariable Integer id){
+        Person person = contactService.retrieve(id);
+        return new ResponseEntity(person, HttpStatus.OK);
+    }
+}
